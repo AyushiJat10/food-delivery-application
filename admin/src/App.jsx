8883f4ce558553a515d625dc6aeb93e0a71be8e0
AyushiJat10 <aayushijat8@gmail.com>
2026-02-09 01:30:13 +0530

    fix casesensitive import path

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-import Sidebar from './Components/Sidebar/sidebar'
+import Sidebar from './Components/Sidebar/Sidebar'
 import { Routes ,Route} from 'react-router-dom'
 import List from './Pages/List/List'
 import Add from './Pages/Add/Add'
