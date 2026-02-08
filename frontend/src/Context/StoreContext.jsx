2d65946324e2e5e682867a0908e3d5c44993f0c9
AyushiJat10 <aayushijat8@gmail.com>
2026-02-09 01:07:49 +0530

    storecontext updated

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext(null)
 
 
 const StoreContextProvider = (props)=>{
-    const url ="http://localhost:4000";
+    const url ="https://food-delivery-application-05ms.onrender.com/";
 
     const [token,setToken]= useState("");
     const [food_list,setFoodList] = useState([]);
