9c43e3ad7622dcb99bae3787c59c42d5a815066d
AyushiJat10 <aayushijat8@gmail.com>
2026-02-09 00:17:04 +0530

    final commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
  export const ConnectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://aayushijat8_db_user:8641074834@cluster1.fkpyp4g.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB Connected"));
 }
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import cartRouter from "./routes/CartRoute.js"
 
 //config
 const app = express()
-const port =4000
+const port = process.env.PORT || 4000;
 
  
 //middleware
